e873067984dd2ec77e3effdc12b22ab900cc59ab
Toocanzs <dupanzszs@gmail.com>
2020-02-25 16:29:25 -0500

    Add wall buffer

--- Assets/scripts/IrradianceProbeManager.cs
+++ Assets/scripts/IrradianceProbeManager.cs
@@ -14,9 +14,17 @@ public class IrradianceProbeManager : MonoBehaviour
     private float probeSeparation = 1f;
     [SerializeField]
     private float2 resetAreaPercent = new float2(0.5f,0.5f);
-    
+    [SerializeField]
+    private Camera lightingCamera = null;
     private float2 OriginOffset => 0.5f * probeSeparation;
 
+    [SerializeField]
+    private int pixelsPerUnit = 32;
+
+    private int2 BufferSize => math.int2(math.float2(probeCounts) * probeSeparation * pixelsPerUnit);
+
+    public RenderTexture buffer;
+    
     void Start()
     {
         if (Instance != null)
@@ -25,36 +33,64 @@ public class IrradianceProbeManager : MonoBehaviour
             return;
         }
         Instance = this;
+
+        var size = BufferSize;
+        buffer = new RenderTexture(size.x, size.y, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        buffer.wrapMode = TextureWrapMode.Clamp;
+        buffer.Create();
     }
-    
+
+    private void OnDestroy()
+    {
+        buffer.Release();
+    }
+
     void Update()
     {
+        float2 center = GetCenter();
         float2 scale = GetWorldScale();
-        float2 dims = GetProbeDims().xy;
-        float2 center = dims + scale / 2;
         float2 resetScale = scale * resetAreaPercent;
         float4 resetBounds = new float4(center - resetScale/2, center + resetScale/2);
 
         float2 cameraPos = math.float3(Camera.main.transform.position).xy;
-
+        
         if (math.any(math.bool4(cameraPos.xy < resetBounds.xy, cameraPos.xy > resetBounds.zw)))
         {
-            transform.position = new float3(cameraPos.xy - scale/2, 0);
+
+            SetCenter(transform, cameraPos.xy);
             //TODO: Transfer valid probe data to new area
         }
+
+        var transform1 = lightingCamera.transform;
+        transform1.position = new float3(center, transform1.position.z);
+        lightingCamera.orthographicSize = scale.y / 2;
+        lightingCamera.aspect = scale.x / scale.y;
     }
 
-    private float2 GetPosition(int2 probePos)
+    void SetCenter(Transform trs, float2 value)
+    {
+        float2 scale = GetWorldScale();
+        trs.position = new float3(value - scale/2, 0);
+    }
+
+    float2 GetCenter()
+    {
+        float2 scale = GetWorldScale();
+        float2 dims = GetProbeAreaDims().xy;
+        return dims + scale / 2;
+    }
+
+    private float2 GetProbePosition(int2 probePos)
     {
         return GetProbeAreaOrigin() + OriginOffset + math.float2(probePos) * probeSeparation;
     }
 
     private float2 GetProbeAreaOrigin()
     {
-        return math.int2(math.float3(transform.position).xy);
+        return math.round(math.float3(transform.position).xy / probeSeparation) * probeSeparation;
     }
     
-    private float4 GetProbeDims()
+    private float4 GetProbeAreaDims()
     {
         float2 origin = GetProbeAreaOrigin();
         return new float4(origin, origin + math.float2(probeCounts) * probeSeparation);
@@ -62,7 +98,7 @@ public class IrradianceProbeManager : MonoBehaviour
 
     private float2 GetWorldScale()
     {
-        var dims = GetProbeDims();
+        var dims = GetProbeAreaDims();
         return new float2(dims.z - dims.x, dims.w - dims.y);
     }
 
@@ -72,14 +108,14 @@ public class IrradianceProbeManager : MonoBehaviour
         {
             for (int x = 0; x < probeCounts.x; x++)
             {
-                var pos = GetPosition(new int2(x,y));
+                var pos = GetProbePosition(new int2(x,y));
                 Gizmos.color = Color.red;
                 Gizmos.DrawSphere(new float3(pos.xy, 0), 0.1f);
             }
         }
 
         float2 scale = GetWorldScale();
-        float2 dims = GetProbeDims().xy;
+        float2 dims = GetProbeAreaDims().xy;
         float2 center = dims + scale / 2;
         
         Gizmos.color = Color.blue;
